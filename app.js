af7b5e4a50ffa4b3fa31848c88588db35f91dd6e
janlankas <lankascb@gmail.com>
2021-07-11 19:26:00 +0200

    start of JS

---
+++ app.js
@@ -0,0 +1,11 @@
+const hikeExp = document.querySelector('.hike-exp');
+
+window.addEventListener('scroll', scrollReveal);
+
+function scrollReveal() {
+    const hikePos = hikeExp.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+    if (hikePos < windowHeight) {
+        hikeExp.style.
+    }
+}
