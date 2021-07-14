75e8280890ec9c748428510be9a14c16472a67ac
janlankas <lankascb@gmail.com>
2021-07-14 14:29:32 +0200

    Scroll + Animation p1

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-const hikeExp = document.querySelector('.hike-exp');
+let controller;
+let slideScene;
 
-window.addEventListener('scroll', scrollReveal);
 
-function scrollReveal() {
-    const hikePos = hikeExp.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-    if (hikePos < windowHeight) {
-        hikeExp.style.
-    }
+function animateSlides() {
+    // Init Controller
+    controller = new ScrollMagic.Controller();
+    //Select 
+    const sliders = document.querySelectorAll('.slide');
+    const nav = document.querySelector('.nav-header');
+    // Loop over each slide
+    sliders.forEach(slide => {
+        const revealImg = slide.querySelector('.reveal-img')
+        const img = slide.querySelector('img');
+        const revealText = slide.querySelector('.reveal-text');
+        //GSAP
+        // gsap.to(revealImg, 1, { x: '100%',});
+        const slideTl = gsap.timeline({
+            defaults: { duration: 1, ease: "power2.inOut" }
+        });
+        slideTl.fromTo(revealImg, { x: '0%' }, { x: "100%" });
+        slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
+        slideTl.fromTo(revealText, { x: '0%' }, { x: "100%" }, "-=0.75");
+        slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+        //Create Scene
+        slideScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            triggerHook: 0.25,
+
+        })
+            .addIndicators({ colorStart: 'white', colorTrigger: 'white', name: 'slide' })
+            .addTo(controller);
+    });
 }
+animateSlides();
