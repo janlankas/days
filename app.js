c73a4706da4f7f8dc14984149e921a360f2d5722
janlankas <lankascb@gmail.com>
2021-07-18 20:46:47 +0200

    scroll div fix

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ function animateSlides() {
         const img = slide.querySelector('img');
         const revealText = slide.querySelector('.reveal-text');
         //GSAP
-        // gsap.to(revealImg, 1, { x: '100%',});
         const slideTl = gsap.timeline({
             defaults: { duration: 1, ease: "power2.inOut" }
         });
@@ -28,15 +27,20 @@ function animateSlides() {
             triggerHook: 0.25,
             reverse: false
 
-        }).setTween(slideTl)
-            .addIndicators({ colorStart: 'white', colorTrigger: 'white', name: 'slide' })
+        })
+            .setTween(slideTl)
+            .addIndicators({
+                colorStart: 'white',
+                colorTrigger: 'white',
+                name: 'slide'
+            })
             .addTo(controller);
         //New animation 
         const pageTl = gsap.timeline();
         let nextSlide = slides.length - 1 === index ? 'end' : slides[index + 1];
         pageTl.fromTo(nextSlide, { y: '0%' }, { y: '50%' });
         pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 })
-        pageTl.fromTo(nextSlide, { y: '50%' }, { y: '0%' });
+        pageTl.fromTo(nextSlide, { y: '50%' }, { y: '0%' }, "-=0.5");
 
         //Create new scene
         pageScene = new ScrollMagic.Scene({
@@ -45,7 +49,7 @@ function animateSlides() {
             triggerHook: 0
 
         })
-            .addIndicators({ colorStart: 'white', colorTrigger: 'white', name: 'page', indent: 200 })
+            .addIndicators({ colorStart: 'blue', colorTrigger: 'white', name: 'page', indent: 200 })
             .setPin(slide, { pushFollowers: false })
             .setTween(pageTl)
             .addTo(controller);
